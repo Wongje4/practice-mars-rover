b288b66b75e843453006928dc333fc32bb920e4a
Wongje4 <>
2022-12-04 12:03:48 +0800

    test: should_01N_when_executeCommand_given_00NM

--- src/main/java/com/afs/tdd/MarsRover.java
+++ src/main/java/com/afs/tdd/MarsRover.java
@@ -1,4 +1,29 @@
 package com.afs.tdd;
 
 public class MarsRover {
+    private Position position;
+    private String command;
+
+
+    public MarsRover(Position position, String command) {
+        this.position = position;
+        this.command = command;
+    }
+
+    public void executeCommand(Position position, String command) {
+
+    }
+
+    public Position getPosition() {
+        return position;
+    }
+
+    public void setPosition(Position position) {
+        this.position = position;
+    }
+
+
+    public void setCommand(String command) {
+        this.command = command;
+    }
 }
--- src/test/java/com/afs/tdd/MarsRoverTest.java
+++ src/test/java/com/afs/tdd/MarsRoverTest.java
@@ -2,9 +2,42 @@ package com.afs.tdd;
 
 import org.junit.jupiter.api.Test;
 
-class MarsRoverTest {
-    @Test
-    void write_your_first_test() {
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+class MarsRoverTest {
+
+    // 00NM -> 01N
+    @Test
+    void should_01N_when_executeCommand_given_00NM() {
+        //given
+
+        Position position = new Position(0, 0, "N");
+        String command = "M";
+
+
+        //when
+        MarsRover marsRover = new MarsRover(position, command);
+        marsRover.executeCommand(position, command);
+
+        //then
+        assertEquals(0, marsRover.getPosition().getX());
+        assertEquals(1, marsRover.getPosition().getY());
+        assertEquals("N", marsRover.getPosition().getDirection());
     }
+    // 00NL -> 00W
+    // 00NR -> 00E
+
+    // 00SM -> 0-1S
+    // 00SL -> 00E
+    // 00SR -> 00W
+
+    // 00EM -> 10E
+    // 00EL -> 00N
+    // 00ER -> 00S
+
+    // 00WM -> -10W
+    // 00WL -> 00S
+    // 00WR -> 00N
+
+
 }
