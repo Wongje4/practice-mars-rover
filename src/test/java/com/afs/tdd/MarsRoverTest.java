56a7560678455849fc79700e3496fc3cf21b3743
Wongje4 <>
2022-12-04 12:45:44 +0800

    test: should_00E_when_executeCommand_given_00SL

--- src/test/java/com/afs/tdd/MarsRoverTest.java
+++ src/test/java/com/afs/tdd/MarsRoverTest.java
@@ -80,6 +80,23 @@ class MarsRoverTest {
         assertEquals("S", marsRover.getPosition().getDirection());
     }
     // 00SL -> 00E
+    @Test
+    void should_00E_when_executeCommand_given_00SL() {
+        //given
+
+        Position position = new Position(0, 0, "S");
+        String command = "L";
+
+
+        //when
+        MarsRover marsRover = new MarsRover(position, command);
+        marsRover.executeCommand(position, command);
+
+        //then
+        assertEquals(0, marsRover.getPosition().getX());
+        assertEquals(0, marsRover.getPosition().getY());
+        assertEquals("E", marsRover.getPosition().getDirection());
+    }
     // 00SR -> 00W
 
     // 00EM -> 10E
